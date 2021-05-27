95853ed584c87d9a3ffe86a4d7e5a269c1f44bff
Wawan Hartawan <harta@gimanamas.com>
2021-05-27 19:52:17 +0700

    <wh> update midlle ware to support cors

---
+++ app/Http/Controllers/ProdukAPI.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Produk;
+use Validator;
+
+class ProdukAPI extends Controller
+{
+    public function getProdukList(){
+        $produk=Produk::where('produk_status','1')->get();
+        return response()->json($produk,200);
+    }
+    public function getProdukById($id){
+        $produk=Produk::where('id',$id)->first();
+        return $produk;
+    }            
+    #menyimpan data produk beserta detail isinya
+    public function produkSimpan(Request $request){
+        $validator = Validator::make($request->all(), [
+            'produk_nama' => ['required','string','max:30'],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['status' => 'error', 'message' => $validator->messages()->first(), 'code' => 404]);
+        }
+
+        $produk=new Produk;
+        $produk->produk_nama=$request->get('produk_nama');
+        $produk->produk_deskripsi=$request->get('produk_deskripsi');
+        $produk->produk_lokasi_gambar=$request->get('produk_lokasi_gambar');
+        $produk->produk_lokasi_video=$request->get('produk_lokasi_video');
+        $produk->produk_harga=$request->get('produk_harga');
+        $produk->produk_stok=$request->get('produk_stok');
+        $produk->produk_status='1'; //aktif
+        $produk->save();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,27 +16,33 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('user/login','userAPI@userLogin');
-Route::put('user/change/password','userAPI@userChangePassword');
+Route::middleware(['cors'])->group(function () {
+    Route::post('user/login','userAPI@userLogin');
+    Route::put('user/change/password','userAPI@userChangePassword');
+    
+    Route::post('donatur/register','DonaturAPI@donaturRegister');
+    Route::post('donatur/register/sosmed','DonaturAPI@donaturRegisterSosmed');
+    Route::put('donatur/update/profile/{id}','DonaturAPI@donaturUpdateProfile');
+    Route::get('donatur/byemail/{email}','DonaturAPI@donaturByEmail');
+    Route::get('donatur/list','DonaturAPI@donaturList');
+    Route::get('donatur/byid/{id}','DonaturAPI@donaturById');
+    
+    Route::post('santri/register','SantriAPI@santriRegister');
+    Route::post('santri/register/sosmed','SantriAPI@santriRegisterSosmed');
+    Route::put('santri/update/profile/{id}','SantriAPI@santriUpdateProfile');
+    Route::get('santri/byemail/{email}','SantriAPI@santriByEmail');
+    Route::get('santri/list','SantriAPI@santriList');
+    Route::get('santri/byid/{id}','SantriAPI@santriById');
+    
+    Route::post('produk/save','ProdukAPI@produkSimpan');
+    Route::get('produk/list','ProdukAPI@getProdukList');
+    Route::get('produk/byid/{id}','ProdukAPI@getProdukById');
+    
+    
+    Route::post('kuesioner/simpan','KuesionerAPI@kuesionerSimpan');
+    Route::get('kuesioner/list','KuesionerAPI@kuesionerList');
+    Route::post('kuesioner/santri/simpan','KuesionerAPI@kuesionerSantriSimpan');
+});
 
-Route::post('donatur/register','DonaturAPI@donaturRegister');
-Route::post('donatur/register/sosmed','DonaturAPI@donaturRegisterSosmed');
-Route::put('donatur/update/profile/{id}','DonaturAPI@donaturUpdateProfile');
-Route::get('donatur/byemail/{email}','DonaturAPI@donaturByEmail');
-Route::get('donatur/list','DonaturAPI@donaturList');
-Route::get('donatur/byid/{id}','DonaturAPI@donaturById');
 
-Route::post('santri/register','SantriAPI@santriRegister');
-Route::post('santri/register/sosmed','SantriAPI@santriRegisterSosmed');
-Route::put('santri/update/profile/{id}','SantriAPI@santriUpdateProfile');
-Route::get('santri/byemail/{email}','SantriAPI@santriByEmail');
-Route::get('santri/list','SantriAPI@santriList');
-Route::get('santri/byid/{id}','SantriAPI@santriById');
 
-Route::post('kuesioner/simpan','KuesionerAPI@kuesionerSimpan');
-Route::get('kuesioner/list','KuesionerAPI@kuesionerList');
-Route::post('kuesioner/santri/simpan','KuesionerAPI@kuesionerSantriSimpan');
-
-Route::get('produk/list','Produk@getProdukList');
-Route::get('produk/byid/{id}','Produk@getProdukById');
-Route::put('produk/save/','Produk@saveProduk');
